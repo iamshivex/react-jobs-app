284557255c40f658e45ba25567b28c4ec454438a
Sheriff <iamshivex04@gmail.com>
2024-09-03 04:41:23 +0100

    iterated another example of useEffect @ JobPage

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,30 @@
 import {React, useState, useEffect} from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner'
+
 
 const JobListings = ({isHome = false}) => {
-  const jobListings = isHome 
-    ? jobs.slice(0, 3) 
-    : jobs;
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome
+        ? "/api/jobs?_limit=3"
+        : "/api/jobs"
+      try {
+        const res = await fetch(apiUrl)
+        const data = await res.json()
+        setJobs(data)
+      } catch (error) {
+        console.log("Error getching data", error)
+      } finally {
+        setLoading(false)
+      }  
+    }
+
+    fetchJobs()
+  }, [])
   
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -13,11 +33,14 @@ const JobListings = ({isHome = false}) => {
           ? "Recent Jobs" 
           : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job}/>
-          ))}
-        </div>
+          {loading 
+            ? <Spinner loading={loading}/>
+            : <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map((job) => (
+                  <JobListing key={job.id} job={job}/>
+                ))}
+              </div>
+          }
       </div>
     </section>
   )
